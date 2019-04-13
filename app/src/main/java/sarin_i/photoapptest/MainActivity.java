7f5c332574f5e52bdbad1bf5f0ea0527f7f0a3ca
Sarin Iamsangtham <Iamsangsam@gmail.com>
2019-04-13 12:04:27 -0400

    More Progess
    Made the basic functionality of the test app finally working, need to work on a few more things though
    - Image displaying in Landscape mode no matter what
    - Potential issues in filling up the storage too much

--- app/src/main/java/sarin_i/photoapptest/MainActivity.java
+++ app/src/main/java/sarin_i/photoapptest/MainActivity.java
@@ -4,6 +4,7 @@ import android.annotation.TargetApi;
 import android.content.Intent;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
+import android.media.ExifInterface;
 import android.net.Uri;
 import android.os.Build;
 import android.os.Bundle;
@@ -30,7 +31,7 @@ public class MainActivity extends AppCompatActivity{
 
     private ImageView imageView;
     
-
+    private ExifInterface exifInterface;
 
 
     private Button btnCapture;
@@ -72,17 +73,21 @@ public class MainActivity extends AppCompatActivity{
     }
 
 
+    //Make the name of the file so that it doesn't collide with any existing photo in the storage
     private File createImageFile() throws IOException{
-
-        //Making the name
         String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
         String imageName = "JPEG_" + timeStamp + "_";
+
+        //Get an external directory for PICTURES
         File storageDir = getExternalFilesDir(Environment.DIRECTORY_PICTURES);
 
+        //Make a temporary file to store in there
         File file = File.createTempFile(imageName, ".jpg", storageDir);
 
-        currentImagePath = storageDir.getAbsolutePath();
+        //Save a path to the current image for use later on
+        currentImagePath = file.getAbsolutePath();
         Log.d("imageFile creation", "THE DIRECTORY IS: " + storageDir);
+
         return file;
     }
 
@@ -94,7 +99,9 @@ public class MainActivity extends AppCompatActivity{
         //Make sure there's a camera
         if (takePictureIntent.resolveActivity(getPackageManager()) != null){
 
+            //Make a file object for the photo
             File photoFile = null;
+
             try{
                 Log.d("dispatchIntent", "CREATING IMAGE FILE");
                 photoFile = createImageFile();
@@ -103,13 +110,14 @@ public class MainActivity extends AppCompatActivity{
                 e.printStackTrace();
             }
 
-
             if (photoFile != null){
+                //Get the photo's URI
                 Log.d("dispatchIntent", "FILE NOT NULL");
-                Uri outputFileUri = FileProvider.getUriForFile(this, "sarin_i.photoapptest.fileprovider", photoFile);
+                Uri photoURI = FileProvider.getUriForFile(this, "sarin_i.photoapptest.fileprovider", photoFile);
 
-                takePictureIntent.putExtra(MediaStore.EXTRA_OUTPUT, outputFileUri);
 
+
+                takePictureIntent.putExtra(MediaStore.EXTRA_OUTPUT, photoURI);
 
                 Log.d("dispatchIntent", "STARTING CAMERA APP");
                 startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE);
@@ -127,16 +135,25 @@ public class MainActivity extends AppCompatActivity{
         Bitmap imageBitMap = null;
         if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == RESULT_OK){
             Log.d("activityResult", "CAMERA CAME BACK ALRIGHT");
+
             File imgFile = new File(currentImagePath);
             if (imgFile.exists()){
                 imageBitMap = BitmapFactory.decodeFile(imgFile.getAbsolutePath());
 
             }
 
+            try {
+                exifInterface = new ExifInterface(currentImagePath);
+
+                int orientation = exifInterface.getAttributeInt(ExifInterface.TAG_ORIENTATION, 1);
+
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
 
             imageView.setImageBitmap(imageBitMap);
         }
     }
-
 
 }
